ecfa2782f1c90e96dc079e080584b8ca91055c07
Lucas Martins <lucasmartinscaet@hotmail.com>
2025-09-09 20:47:52 -0300

    feat: wip coffee-card

--- src/components/home/coffee-card.tsx
+++ src/components/home/coffee-card.tsx
@@ -1,25 +1,42 @@
 import type { ICoffeeCard } from '../../@types/coffee-card-type'
-import { ShoppingCartIcon } from '@phosphor-icons/react'
+import { ShoppingCartSimpleIcon } from '@phosphor-icons/react'
 
 export function CoffeeCard({ id, name, description, categories, price, image }: ICoffeeCard) {
   return (
-    <div className="w-3xs h-80 relative">
+    <div className="h-80 relative">
       <div className="flex flex-col items-center absolute">
         <img src={image} alt="Ícone do café" />
-        <p>{categories}</p>
-        <p>{name}</p>
-        <p>{description}</p>
-        <div className="centered">
-          <p>{price}</p>
-          <div>
+        <div className="centered gap-1">
+          {
+            categories.map((category) => {
+              return (
+                <p className="mt-3 px-2 py-1 rounded-full bg-yellow-light font-bold text-[10px] text-yellow-dark">{category.toUpperCase()}</p>
+              )
+            })
+          }
+        </div>
+
+        <p className="mt-4 font-baloo font-bold text-xl text-base-subtitle">{name}</p>
+        <p className="mt-2 mx-5 text-base-label text-center text-[0.875rem]">{description}</p>
+        {/* Buy section */}
+        {/* FIXME Justify incorreto */}
+        <section className="mt-8 w-52 h-9 flex justify-between items-center gap-6 border">
+          <span className="centered gap-1">
+            <p className="text-[0.875rem] text-base-text">R$</p>
+            <p className="font-baloo font-extrabold text-2xl text-base-text">{price.toFixed(2).replace('.', ',')}</p>
+          </span>
+          <div className="centered gap-2">
             {/* será que é melhor fazer de outra forma ? */}
-            <input type="number" name="" id="" />
+            <span className="w-18 border">
+              OI
+            </span>
+
             <button>
-              <ShoppingCartIcon weight="fill" />
+              <ShoppingCartSimpleIcon weight="fill" className="p-2 size-9 bg-purple-dark text-base-card rounded-[6px]" />
             </button>
           </div>
 
-        </div>
+        </section>
       </div>
       <div className="bg-base-card mt-5 h-[19.375rem] rounded-tl-[6px] rounded-br-[6px] rounded-tr-[2.25rem] rounded-bl-[2.25rem]"></div>
     </div>
