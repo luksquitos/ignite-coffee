8e30e4df8c5a50f381dd9381f69a27f68c189515
Lucas Martins <lucasmartinscaet@hotmail.com>
2025-09-10 20:48:37 -0300

    feat: creating cart items number
    fix: padding on cart button

--- src/components/shared/header.tsx
+++ src/components/shared/header.tsx
@@ -3,7 +3,6 @@ import { NavLink } from 'react-router-dom'
 import igniteLogo from '../../assets/logo.svg'
 
 export function Header() {
-  // TODO Implementar balão flutuante que indica quantos itens o usuário tem no carrinho.
   return (
     <header className="py-8 flex items-center justify-between centered-box">
       <NavLink to="/">
@@ -14,7 +13,10 @@ export function Header() {
           <MapPinIcon weight="fill" className="text-purple" size={22} />
           Porto Alegre, RS
         </button>
-        <NavLink to="/payment" className="bg-yellow-light rounded px-[10px] py-2">
+
+        <NavLink to="/payment" className="bg-yellow-light relative rounded p-2.5">
+          {/* Butão flutuante de itens */}
+          {/* <div className="bg-yellow-dark rounded-full size-5 absolute -top-1.5 -right-2.5 centered text-white font-bold text-xs">3</div> */}
           <ShoppingCartIcon weight="fill" className="text-yellow-dark text-2xl hover:cursor-pointer" />
         </NavLink>
       </div>
