79f4c7f42c54b11e1bbbdfbe1420330610400834
Lucas Martins <lucasmartinscaet@hotmail.com>
2025-09-15 18:42:28 -0300

    feat: finishing form for address

--- src/components/payment/address-form.tsx
+++ src/components/payment/address-form.tsx
@@ -5,18 +5,30 @@ export function AddressForm() {
   return (
     <div className="centered-box mt-10! rounded-md">
       <p className="text-base-subtitle text-lg font-bold">Complete seu pedido</p>
-      <form action="" className="mt-4 p-10 w-[40rem] h-[23.25rem] bg-base-card">
+      <form action="" className="mt-4  rounded-md p-10 w-[40rem] h-[23.25rem] bg-base-card">
         <main className="">
           <div className="flex items-start gap-2">
             <MapPinLineIcon className="size-5.5 text-yellow-dark" />
-            <div className="">
+            <div>
               <p className="text-base-subtitle">Endereço de entrega</p>
               <p className="text-base-text text-sm">Informe o endereço onde deseja receber seu pedido </p>
             </div>
           </div>
           {/* Inputs */}
-          <div>
-            <Input placeholder="CEP" />
+          <div className="mt-8 grid grid-cols-1 gap-4">
+            <div className="grid grid-cols-[2fr_3fr_1fr]">
+              <Input placeholder="CEP" className="h-10" />
+            </div>
+            <Input placeholder="Rua" className="h-10" />
+            <div className="grid grid-cols-1 lg:grid-cols-[1fr_2fr] gap-3">
+              <Input placeholder="Número" className="h-10" />
+              <Input placeholder="Complemento" className="h-10" />
+            </div>
+            <div className="grid grid-cols-1 lg:grid-cols-[2fr_3fr_1fr] gap-3">
+              <Input placeholder="Bairro" className="h-10" />
+              <Input placeholder="Cidade" className="h-10" />
+              <Input placeholder="UF" className="h-10" />
+            </div>
           </div>
         </main>
       </form>
