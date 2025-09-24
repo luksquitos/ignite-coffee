dadb28319588206edc72dcd966ca3cee73ae1236
Lucas Martins <lucasmartinscaet@hotmail.com>
2025-09-24 16:16:41 -0300

    refact: CartItem component receives the cartItem object instead  of  object properties feat: remove button receives a function to delete a item from cart state

--- src/components/payment/cart-item.tsx
+++ src/components/payment/cart-item.tsx
@@ -1,23 +1,26 @@
 import type { ICartItem } from '@/@types/cart-item-type'
 import { TrashIcon } from '@phosphor-icons/react'
 import { useState } from 'react'
+import { useCart } from '@/providers/cart-provider'
 import { Counter } from '../ui/counter'
 
-export function CartItem({ coffee, quantity }: ICartItem) {
-  const [currentQuantity, setCurrentQuantity] = useState(quantity)
+export function CartItem({ cartItem }: { cartItem: ICartItem }) {
+  const [currentQuantity, setCurrentQuantity] = useState(cartItem.quantity)
+  const { removeCoffeeFromCart } = useCart()
+
   return (
     <div className="centered w-full! px-1 py-2 gap-12.5 items-start! ">
       <div className="centered gap-5">
-        <img src={coffee.image} alt="Ícone do Café" className="size-16" />
+        <img src={cartItem.coffee.image} alt="Ícone do Café" className="size-16" />
         <div className="flex flex-col gap-2">
-          <p className="text-base-subtitle">{coffee.name}</p>
+          <p className="text-base-subtitle">{cartItem.coffee.name}</p>
           <div className="centered gap-2">
             <Counter
-              value={quantity}
+              value={cartItem.quantity}
               textMinor
             />
 
-            <button className="centered bg-base-button gap-1 rounded-md p-2 hover:bg-base-hover">
+            <button onClick={() => removeCoffeeFromCart(cartItem)} className="centered bg-base-button gap-1 rounded-md p-2 hover:bg-base-hover">
               <TrashIcon className="text-purple" />
               <p className="text-base-text text-xs">REMOVER</p>
             </button>
@@ -27,7 +30,7 @@ export function CartItem({ coffee, quantity }: ICartItem) {
       </div>
       <p className="font-bold text-base-text">
         R$
-        {(coffee.price * quantity).toFixed(2).replace('.', ',')}
+        {(cartItem.coffee.price * cartItem.quantity).toFixed(2).replace('.', ',')}
       </p>
     </div>
   )
