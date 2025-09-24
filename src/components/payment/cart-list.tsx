272d740d2c033f1918392715865399f68757c4bb
Lucas Martins <lucasmartinscaet@hotmail.com>
2025-09-24 16:49:20 -0300

    feat: adding 'sum' section only when cart has items

--- src/components/payment/cart-list.tsx
+++ src/components/payment/cart-list.tsx
@@ -3,8 +3,9 @@ import { useCart } from '@/providers/cart-provider'
 import { CartItem } from './cart-item'
 
 export function CartList() {
-  const { cart } = useCart()
+  const { cart, cartHasItems } = useCart()
   const { cartItemsSum } = useReducer((item) => {})
+
   return (
 
     <div className="centered-box mt-10!">
@@ -15,34 +16,36 @@ export function CartList() {
           cart.map((item) => {
             return (
               <>
-                <CartItem
-                  coffee={item.coffee}
-                  quantity={item.quantity}
-                />
+                <CartItem cartItem={item} />
                 <span className="block my-6 border-t-1" />
               </>
             )
           })
         }
         {/* Soma */}
-        <div>
-          <div className="flex justify-between">
-            <p className="text-base-text">Total de Itens</p>
-            <p className="text-base-text">R$ 20,00</p>
+        {cartHasItems && (
+          <div>
+            <div className="flex justify-between">
+              <p className="text-base-text">Total de Itens</p>
+              <p className="text-base-text">R$ 20,00</p>
+            </div>
+            <div className="mt-3 flex justify-between">
+              <p className="text-base-text">Entrega</p>
+              <p className="text-base-text">R$ 4,50</p>
+            </div>
+            <div className="mt-3 flex justify-between">
+              <p className="font-bold text-xl text-base-subtitle">Total</p>
+              <p className="font-bold text-xl text-base-subtitle">R$ 24,50</p>
+            </div>
           </div>
-          <div className="mt-3 flex justify-between">
-            <p className="text-base-text">Entrega</p>
-            <p className="text-base-text">R$ 4,50</p>
-          </div>
-          <div className="mt-3 flex justify-between">
-            <p className="font-bold text-xl text-base-subtitle">Total</p>
-            <p className="font-bold text-xl text-base-subtitle">R$ 24,50</p>
-          </div>
-        </div>
 
-        <button className="mt-6 w-full py-2.5 bg-yellow text-white font-bold text-sm rounded-md hover:bg-yellow-dark hover:cursor-pointer transition">
-          CONFIRMAR PEDIDO
-        </button>
+        )}
+
+        {cartHasItems && (
+          <button className="mt-6 w-full py-2.5 bg-yellow text-white font-bold text-sm rounded-md hover:bg-yellow-dark hover:cursor-pointer transition">
+            CONFIRMAR PEDIDO
+          </button>
+        )}
 
       </section>
 
